e173ac8cf8f43ec260ddfd5d4e7aa1dc343afd2e
Gusseppe <hackersvlc@gmail.com>
2025-02-17 09:46:00 -0500

    change mlflow app name to mlflow-agentes

--- mlflow/uninstall.sh
+++ mlflow/uninstall.sh
@@ -4,7 +4,7 @@ set -e
 echo "🔄 Uninstalling MLflow..."
 
 if helm list | grep -q "mlflow"; then
-  helm uninstall mlflow
+  helm uninstall mlflow-agentes
   echo "✅ MLflow uninstalled successfully!"
 else
   echo "⚠️ MLflow is not installed. Skipping uninstallation."
